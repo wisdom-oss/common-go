b4747878821e5f11b959f88c5a41330d7f8590ee
Jan Eike Suchard <jan@suchard.cloud>
2024-10-16 13:20:47 +0200

    changed collection method to only output one error instead of many

--- middleware/error-handler.go
+++ middleware/error-handler.go
@@ -109,18 +109,14 @@ func (h ErrorHandler) Handler(next http.Handler) http.Handler {
 // context if that didn't already happen
 func (h ErrorHandler) Gin(c *gin.Context) {
 	c.Next()
-	var errors []types.ServiceError
+	errorResponse := errorHandler.InternalError
+	var errors []error
 	for _, err := range c.Errors {
-		errors = append(errors, types.ServiceError{
-			Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.6.1",
-			Status: 500,
-			Title:  "Internal Server Error",
-			Detail: "The service encountered an internal error during the handling of your request",
-			Errors: []error{err.Err},
-		})
+		errors = append(errors, err.Err)
 	}
+	errorResponse.Errors = errors
 	if len(errors) > 0 && c.IsAborted() {
 		c.Header("Content-Type", "application/problem+json; charset=utf-8")
-		c.JSON(500, errors)
+		c.JSON(500, errorResponse)
 	}
 }
