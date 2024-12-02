28ffe81d10675c3587a2228c714c850acd745fae
Jan Eike Suchard <jan@suchard.cloud>
2024-12-02 12:30:41 +0100

    removing old middlewares

--- middleware/errorHandler/error-handler_consts.go
+++
@@ -1,7 +0,0 @@
-package errorHandler
-
-import internal "github.com/wisdom-oss/common-go/v2/internal/error-handler"
-
-type contextKey string
-
-var ErrorChannelName = contextKey(internal.ErrorChannelName)
