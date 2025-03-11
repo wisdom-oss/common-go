f96dc902328718880feb4c78e2f3cdc7080070ff
Jan Eike Suchard <jan@suchard.cloud>
2025-03-11 10:36:26 +0100

    chore: changing ints to constants and preallocating arrays

--- middleware/gin/error-handler/handler.go
+++ middleware/gin/error-handler/handler.go
@@ -1,13 +1,16 @@
 package errorHandler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+
 	"github.com/wisdom-oss/common-go/v3/types"
 )
 
 var InternalError = &types.ServiceError{
 	Type:   "https://www.rfc-editor.org/rfc/rfc9110#section-15.6.1",
-	Status: 500,
+	Status: http.StatusInternalServerError,
 	Title:  "Internal Server Error",
 	Detail: "The service encountered an internal error during the handling of your request",
 }
@@ -15,13 +18,13 @@ var InternalError = &types.ServiceError{
 func Handler(c *gin.Context) {
 	c.Next()
 	errorResponse := InternalError
-	var errors []error
-	for _, err := range c.Errors {
-		errors = append(errors, err.Err)
+	errs := make([]error, len(c.Errors))
+	for idx, err := range c.Errors {
+		errs[idx] = err.Err
 	}
-	errorResponse.Errors = errors
-	if len(errors) > 0 && c.IsAborted() {
+	errorResponse.Errors = errs
+	if len(errs) > 0 && c.IsAborted() {
 		c.Header("Content-Type", "application/problem+json; charset=utf-8")
-		c.JSON(500, errorResponse)
+		c.JSON(http.StatusInternalServerError, errorResponse)
 	}
 }
